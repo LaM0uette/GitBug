639bb77339885b8b6252c8b7809486a11162cd6d
LaM0uette <LaM0uette.dev@gmail.com>
2025-05-11 15:34:27 +0200

    modify grid size

--- GitBug/GitBug.Client/Pages/Game.razor.cs
+++ GitBug/GitBug.Client/Pages/Game.razor.cs
@@ -1,4 +1,5 @@
-﻿using GitBug.Shared;
+﻿using System.Text.Json;
+using GitBug.Shared;
 using Microsoft.AspNetCore.Components;
 
 namespace GitBug.Client;
@@ -10,27 +11,17 @@ public class GameBase : ComponentBase
     [Parameter] public string UserName { get; set; } = string.Empty;
     [Parameter] public int SelectedYear { get; set; }
     
-    protected Cell[,] Grid = null!;
+    protected Cell[,]? Grid;
     
     [Inject] private HttpClient _httpClient { get; set; } = null!;
     [Inject] private UIStore _uiStore { get; set; } = null!;
     
-    private IReadOnlyDictionary<int, int> _totalByYears = null!;
-    private IReadOnlyList<ContributionDTO> _contributions = null!;
+    private IReadOnlyDictionary<int, int>? _totalByYears;
+    private IReadOnlyList<ContributionDTO>? _contributions;
     
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-        var contributionsState = _uiStore.GetState<ContributionsState>();
-        
-        if (string.IsNullOrWhiteSpace(UserName))
-            UserName = contributionsState.UserName;
-        
-        if (SelectedYear == 0)
-            SelectedYear = contributionsState.Year;
-        
-        _totalByYears = contributionsState.TotalByYears;
-        _contributions = contributionsState.Contributions;
-        
+        await FetchData();
         CreateNewGame();
     }
 
@@ -40,6 +31,9 @@ public class GameBase : ComponentBase
 
     private void CreateNewGame()
     {
+        if (_totalByYears == null || _contributions == null)
+            return;
+        
         List<ContributionDTO> filteredContributions = _contributions.Where(c => c.Date.Year == SelectedYear).ToList();
         if (filteredContributions.Count == 0)
             return;
@@ -62,6 +56,36 @@ public class GameBase : ComponentBase
                 contributions.TryGetValue(date, out ContributionDTO contribution);
                 Grid[week, day] = new Cell(date, contribution.Level);
             }
+        }
+    }
+    
+    private async Task FetchData()
+    {
+        if (string.IsNullOrWhiteSpace(UserName))
+            return;
+        
+        var url = $"https://github-contributions-api.jogruber.de/v4/{UserName}";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.UserAgent.ParseAdd("GitBug");
+
+        HttpResponseMessage response = await _httpClient.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            var contributionsData = JsonSerializer.Deserialize<ContributionsDTO>(content);
+            
+            _uiStore.Dispatch(new SetContributionsAction(UserName, SelectedYear, contributionsData.TotalByYears, contributionsData.Contributions));
+            var contributionsState = _uiStore.GetState<ContributionsState>();
+            
+            _totalByYears = contributionsState.TotalByYears;
+            _contributions = contributionsState.Contributions;
+        }
+        else
+        {
+            _totalByYears = null;
+            _contributions = null;
+            
+            Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
 
